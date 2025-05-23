d08ce77337e4ac35fe9882545a5c39ec72abc266
Miky8745 <mikulas.vanecek@gmail.com>
2025-05-23 17:37:28 +0200

    Changed installer logic

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ chmod +x env.sh
 chmod +x ar.sh
 chmod +x vr.sh
 
+git clone https://github.com/FAV-SmartGlasses/SmartGlasses
+
 python3 -m venv venv
 source env.sh
 
-git clone https://github.com/FAV-SmartGlasses/SmartGlasses
 cd SmartGlasses
 pip install -r requirements_rpi.txt
 cd ..
